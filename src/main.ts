3d5b967417bbd0151d2d689d24b07c7632e0fc2f
Chandlerzz <Chandlerzz@github.com>
2022-07-06 17:13:41 +0800

    登录页制作

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vant, { Toast } from "vant"
+import router from './router'
+import {store,key} from './store'
+import "@/assets/styles/index.scss"
+// import store from "./store/index"
 
 createApp(App).mount('#app')
+
+
+const app = createApp(App)
+
+app.config.globalProperties.$toast = (msg: string) => {
+    return Toast(msg)
+}
+app.config.globalProperties.$msg = (msg: string) => {
+    return ElMessage(msg)
+}
+app
+  .use(store,key)
+  .use(router)
+  .use(vant)
+  .mount("#app")
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+import { join } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
+  server:{
+    port:3040,
+    proxy:{
+      '/api/':{
+        target:'ttps://esbtest.risen.com/dev/risen_out_management/',
+        changeOrigin:true,
+        secure:false,
+      }
+    },
+    hmr:{
+      overlay:false,
+    }
+  },
+  resolve:{
+    alias:{
+      'vue': 'vue/dist/vue.esm-bundler.js',
+     '@': join(__dirname, "src"),
+    }
+  },
 })
+
